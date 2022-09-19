441423d67320d1cd3e331575defb7497e9f8f04f
RWR <rwr8106@gmail.com>
2022-09-18 22:27:11 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle);
+
+mainTitle.innerText = "Yay, JavaScript!!";
+console.log(mainTitle);
+
+var body = document.querySelector("body");
+console.log(body);
+
+body.style.backgroundColor = "#FEF2F1";
